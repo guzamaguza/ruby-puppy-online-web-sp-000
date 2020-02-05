d6810c9bf52cdd2d8a59a14da5d11547eec7976c
Mark A Guza <guzamaguza@gmail.com>
2020-02-05 02:31:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+
+class Dog
+
+@@all = []
+
+  def initialize(name)
+    @name = name 
+    name.save
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.print_all  
+    @@all.each do |dog|
+      puts dog 
+    end
+  end
+  
+  def save(name)
+    @@all << self.Dog 
+  end
+
+end
+
